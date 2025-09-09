54998790df1a407a5dd97323682e09521bdc4723
ZemerGrey <declan.s.rice2011@gmail.com>
2025-09-09 16:06:11 -0400

    e

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MonroeRobotics/util/armController.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.MonroeRobotics.util;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class armController {
+
+}
